d7466d5892aef8d440f682952fdc22be42e6350c
ibelyanova <myEmail>
2020-11-02 20:43:15 +0300

    Выделен класс для управления сессиями

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -11,11 +11,11 @@ def app(request):
     return fixture
 
 def test_case(app):
-    app.login(Configurations_login(username="admin", password="secret"))
+    app.session.login(Configurations_login(username="admin", password="secret"))
     app.create_group(Configurations_group(name="Test", header="Test", footer="Test"))
-    app.logout()
+    app.session.logout()
 
 def test_add_empty_group(app):
-    app.login(Configurations_login(username="admin", password="secret"))
+    app.session.login(Configurations_login(username="admin", password="secret"))
     app.create_group(Configurations_group(name="", header="", footer=""))
-    app.logout()
+    app.session.logout()
