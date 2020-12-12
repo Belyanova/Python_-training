b0030381b10d61b659fb6684974a6c75959b9695
ibelyanova <myEmail>
2020-12-12 12:27:26 +0300

    Ошибка при запуске после добавления конфиг файла

--- conftest.py
+++ conftest.py
@@ -1,19 +1,21 @@
 ﻿import pytest
+import json
 from fixture.application import Application
 
 fixture = None
+target = None
 
 @pytest.fixture
 def app(request):
     global fixture
-    if fixture is None:
-        browser=request.config.getoption("--browser")
-        baseUrl = request.config.getoption("--baseUrl")
-        fixture = Application(browser=browser, baseUrl=baseUrl)
-    else:
-        if not fixture.is_valid():
-            fixture = Application(browser=browser, baseUrl=baseUrl)
-    fixture.session.ensure_login(username="admin", password="secret")
+    global target
+    browser = request.config.getoption("--browser")
+    if target is None:
+        with open(request.config.getoption("--target")) as config_file:
+            target = json.load(config_file)
+    if fixture is None or not fixture.is_valid():
+        fixture = Application(browser=browser, baseUrl=target['baseUrl'])
+    fixture.session.ensure_login(username=target['username'], password=target['password'])
     return fixture
 
 @pytest.fixture(scope = "session", autouse = True)
@@ -26,4 +28,4 @@ def stop(request):
 
 def pytest_addoption(parser):
     parser.addoption("--browser", action="store", default="firefox")
-    parser.addoption("--baseUrl", action="store", default="http://localhost/index.php")
+    parser.addoption("--target", action="store", default="target.json")
