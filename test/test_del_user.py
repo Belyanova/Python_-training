9293f1caaea7153011d88b95d601096931584a30
ibelyanova <myEmail>
2020-12-13 12:46:01 +0300

    Тест удаления пользователя готов

--- test/test_del_user.py
+++ test/test_del_user.py
@@ -1,15 +1,17 @@
 ﻿# -*- coding: utf-8 -*-
 from model.configurations_user import Configurations_user
-from random import randrange
+import random
 
-def test_delete_first_group(app, json_users):
+def test_delete_first_group(app, db, check_ui, json_users):
     user = json_users
-    if app.user.count() == 0:
+    if len(db.get_user_list()) == 0:
         app.user.add_new_user(user)
-    old_users = app.user.get_user_list()
-    index = randrange(len(old_users))
-    app.user.delete_user_by_index(index)
+    old_users = db.get_user_list()
+    user = random.choice(old_users)
+    app.user.delete_user_by_id(user.id)
     assert len(old_users) - 1 == app.user.count()
-    new_users = app.user.get_user_list()
-    old_users[index:index+1] = []
+    new_users = db.get_user_list()
+    old_users .remove(user)
     assert old_users == new_users
+    if check_ui:
+        assert sorted(new_users , key=Configurations_user.id_or_max) == sorted(app.user.get_group_list(), key=Configurations_user.id_or_max)
