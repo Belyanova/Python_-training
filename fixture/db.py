174fdfe52786eeade3458ee34945028d169ca831
ibelyanova <myEmail>
2020-12-13 09:35:49 +0300

    Загрузка из БД

--- fixture/db.py
+++ fixture/db.py
@@ -1,4 +1,6 @@
 ﻿import pymysql
+from model.configurations_group import Configurations_group
+
 class DbFixture:
     def __init__(self, host, name, user, password):
         self.host = host
@@ -6,6 +8,18 @@ class DbFixture:
         self.user = user
         self.password = password
         self.connection = pymysql.connect(host=host, database=name , user=user , password=password )
+
+    def get_group_list(self):
+        list = []
+        cursor = self.connection.cursor()
+        try:
+            cursor.execute("select group_id, group_name, group_header, group_footer from group_list")
+            for row in cursor:
+                (id,name,header,footer) = row
+                list.append(Configurations_group(id=str(id), name=name, header=header, footer=footer))
+        finally:
+            cursor.close()
+        return list
 
     def destroy(self):
         self.connection.close()
---
+++ test/test_db_matches_ui.py
@@ -0,0 +1,7 @@
+﻿from model.configurations_group import Configurations_group
+def test_group_list(app, db):
+    ui_list = app.group.get_group_list()
+    def clean (group):
+        return Configurations_group (id=group.id, name=group.name.strip())
+    db_list = map(clean,db.get_group_list())
+    assert sorted(ui_list, key=Configurations_group.id_or_max) == sorted(db_list, key=Configurations_group.id_or_max)
