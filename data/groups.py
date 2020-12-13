1f08d564fa76a55a6ded9af48e6e93c1460e6287
ibelyanova <myEmail>
2020-12-13 08:19:39 +0300

    Реализация генератора и загрузка данных из файла для групп

---
+++ data/groups.py
@@ -0,0 +1,6 @@
+﻿from model.configurations_group import Configurations_group
+
+testdata = [
+    Configurations_group(name="name1", header="header1", footer="footer1"),
+    Configurations_group(name="name2", header="header2", footer="footer2"),
+]
